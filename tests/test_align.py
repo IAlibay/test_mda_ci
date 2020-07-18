8d4b491d5a9d2313f013fdb2dcdfaa941797bd2c
IAlibay <irfan.alibay@gmail.com>
2020-07-18 14:45:15 +0100

    adds rmsf test

---
+++ tests/test_align.py
@@ -0,0 +1,57 @@
+"""
+Unit and regression test for the MDRestraintsGenerator package.
+"""
+
+# Import package, test suite, and other packages as needed
+import MDAnalysis as mda
+from MDAnalysisTests.datafiles import PSF, DCD, TPR, XTC
+from MDAnalysis.analysis import rms, align
+from numpy.testing import assert_almost_equal
+import warnings
+import pytest
+
+
+@pytest.mark.parametrize('run', [
+    1, 2
+])
+def test_rmsf_psf(run):
+    """Align multiple times + RMSF"""
+
+    u = mda.Universe(PSF, DCD)
+
+    average = align.AverageStructure(u, u, select='protein and name CA',
+                                     ref_frame=0).run()
+
+    ref = average.universe
+
+    aligner = align.AlignTraj(u, ref,
+                              select='protein and name CA',
+                              in_memory=True).run()
+
+    c_alphas = u.select_atoms('protein and name CA')
+    R = rms.RMSF(c_alphas).run()
+
+    assert 1 == 1
+
+
+@pytest.mark.parametrize('run', [
+    1, 2
+])
+def test_rmsf_xtc(run):
+    """Align multiple times + RMSF"""
+
+    u = mda.Universe(TPR, XTC)
+
+    average = align.AverageStructure(u, u, select='protein and name CA',
+                                     ref_frame=0).run()
+
+    ref = average.universe
+
+    aligner = align.AlignTraj(u, ref,
+                              select='protein and name CA',
+                              in_memory=True).run()
+
+    c_alphas = u.select_atoms('protein and name CA')
+    R = rms.RMSF(c_alphas).run()
+
+    assert 1 == 1
